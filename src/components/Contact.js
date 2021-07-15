e24d385a5d85437b132d9b72be06325569c6cf39
jsdev90 <28101675+jsdev90@users.noreply.github.com>
2021-07-15 14:47:06 -0400

    EmailJS integration

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
@@ -7,6 +7,13 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Flip from 'react-reveal/Flip';
 import Send from "@material-ui/icons/Send";
+import emailjs, { init } from 'emailjs-com';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const useStyles = makeStyles((theme) => ({
   contactContainer: {
@@ -63,12 +70,35 @@ const InputField = withStyles({
 })(TextField);
 
 const Contact = () => {
+  const [open, setOpen] = useState(false);
   const classes = useStyles();
+  
+  useEffect(() => {
+    init("user_O5HFMB4vdt5pPNPhH3aqS");
+  }, []);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    emailjs.sendForm('service_7w5xua4', 'template_5djxh6u', e.target, 'user_O5HFMB4vdt5pPNPhH3aqS')
+    .then((result) => {
+        if (result.status === 200) {
+          setOpen(true)
+          // const frm = document.getElementsByTagName('form')[0];
+          // frm.submit(); // Submit the form
+          // frm.reset();  // Reset all form data
+          // return false; // Prevent page refresh
+        }
+    }, (error) => {
+        console.log(error.text);
+    });
+  }
+
   return (
     <Box component="div" className={classes.contactContainer}>
       <Flip left>
         <Grid container justify="center">
-          <Box component="form" className={classes.form}>
+          <Box component="form" className={classes.form} onSubmit={sendEmail}>
             <Typography variant="h5" className={classes.heading}>
               Hire or Contact me...
             </Typography>
@@ -77,6 +107,7 @@ const Contact = () => {
               label="Name"
               variant="outlined"
               inputProps={{ className: classes.input }}
+              name="user_name"
             />
             <InputField
               fullWidth={true}
@@ -84,6 +115,8 @@ const Contact = () => {
               variant="outlined"
               inputProps={{ className: classes.input }}
               className={classes.field}
+              type="email"
+              name="user_email"
             />
             <InputField
               fullWidth={true}
@@ -92,18 +125,29 @@ const Contact = () => {
               multiline
               rows={4}
               inputProps={{ className: classes.input }}
+              name="message"
             />
             <Button
               variant="outlined"
               fullWidth={true}
               endIcon={<Send />}
               className={classes.button}
+              type="submit"
             >
               Contact Me
             </Button>
           </Box>
         </Grid>
       </Flip>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={open}
+        onClose={() => setOpen(false)}
+        autoHideDuration={6000}
+        key={"top-right"}
+      >
+        <Alert severity="success">Thanks for contacting me!</Alert>
+      </Snackbar>
     </Box>
   );
 };
