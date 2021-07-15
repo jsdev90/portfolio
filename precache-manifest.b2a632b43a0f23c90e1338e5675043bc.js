4d6e2405f77a6629b54da0b370e7b90feda28c02
jsdev90 <28101675+jsdev90@users.noreply.github.com>
2021-07-15 15:33:53 -0400

    Updates

---
+++ precache-manifest.b2a632b43a0f23c90e1338e5675043bc.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54170e84dd23b852e3330ee6fab209c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6ccc0dfc555ab4ca6812",
+    "url": "/portfolio/static/css/2.22ca87da.chunk.css"
+  },
+  {
+    "revision": "356ba8e35e78a52d04f1",
+    "url": "/portfolio/static/css/main.753f2c85.chunk.css"
+  },
+  {
+    "revision": "6ccc0dfc555ab4ca6812",
+    "url": "/portfolio/static/js/2.98f9b6de.chunk.js"
+  },
+  {
+    "revision": "28d026abe4fbf23a63e9a43ab8c1860e",
+    "url": "/portfolio/static/js/2.98f9b6de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "356ba8e35e78a52d04f1",
+    "url": "/portfolio/static/js/main.df8cfd24.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "b64f53bff4d1b4406145067115b67726",
+    "url": "/portfolio/static/media/Resume.b64f53bf.pdf"
+  },
+  {
+    "revision": "ab7e32a894b27b9a298835dbf39dcdee",
+    "url": "/portfolio/static/media/avatar.ab7e32a8.png"
+  },
+  {
+    "revision": "5398b49d9e1a410454ebe504e4dc61f8",
+    "url": "/portfolio/static/media/background.5398b49d.jpeg"
+  },
+  {
+    "revision": "b2d8ecbacae36b59cbddb27b23af5491",
+    "url": "/portfolio/static/media/campusoptics.b2d8ecba.png"
+  },
+  {
+    "revision": "c3fe2a63c49ddbd54a72017c040e40c8",
+    "url": "/portfolio/static/media/jex.c3fe2a63.png"
+  },
+  {
+    "revision": "1543bb4de43104269f3cb558238a033c",
+    "url": "/portfolio/static/media/lob.1543bb4d.png"
+  },
+  {
+    "revision": "12654f9714e78e5996ef420faf83114e",
+    "url": "/portfolio/static/media/pleroma.social.12654f97.png"
+  },
+  {
+    "revision": "d33d5fcf8d636e80e61c90e8808c3558",
+    "url": "/portfolio/static/media/qxtro-python-staging.herokuapp.com.d33d5fcf.png"
+  },
+  {
+    "revision": "8458b75e3ac64707b1724a22b261157c",
+    "url": "/portfolio/static/media/react.8458b75e.png"
+  },
+  {
+    "revision": "c00c81f0c81eaf5f3f2cadd2ebb4426e",
+    "url": "/portfolio/static/media/rentberry.c00c81f0.jpg"
+  },
+  {
+    "revision": "3601ead2f931d48fc3abfa78a1b3049d",
+    "url": "/portfolio/static/media/rolfsonoil.3601ead2.png"
+  },
+  {
+    "revision": "6f50bccf66167019bdb9a7677dc8cb51",
+    "url": "/portfolio/static/media/vue.6f50bccf.jpeg"
+  },
+  {
+    "revision": "3960ee5755375db8ab427958c071b21c",
+    "url": "/portfolio/static/media/web.3960ee57.png"
+  },
+  {
+    "revision": "1fdaa6e438411eb5dd461fcfa093a17f",
+    "url": "/portfolio/static/media/webflow.1fdaa6e4.png"
+  },
+  {
+    "revision": "e483d3867148c4791eb270349f51062e",
+    "url": "/portfolio/static/media/worklete.e483d386.png"
+  },
+  {
+    "revision": "3560e805e78701054f8015beb32971aa",
+    "url": "/portfolio/static/media/www.archetape.com.3560e805.jpg"
+  },
+  {
+    "revision": "0fad5e890940218007fd133587fa547b",
+    "url": "/portfolio/static/media/www.assured.claims.0fad5e89.png"
+  },
+  {
+    "revision": "a9cc5e14dbc409d8b8cefb56c7bd2b34",
+    "url": "/portfolio/static/media/www.loungebuddy.com.a9cc5e14.png"
+  },
+  {
+    "revision": "ea955ebe2ae4612d9563cde088bdad1f",
+    "url": "/portfolio/static/media/www.modzy.com.ea955ebe.png"
+  },
+  {
+    "revision": "617b83f029171944ab2496e76f016e8f",
+    "url": "/portfolio/static/media/www.ravetree.com.617b83f0.png"
+  },
+  {
+    "revision": "5c6e397fb2343e5734ce3f8c1baa33bc",
+    "url": "/portfolio/static/media/xangular_react_icon.5c6e397f.png"
+  }
+]);
